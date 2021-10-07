5f1415c309b1b48d048427d9e4a37cd7e8ada8a4
Tucker Brown <browntuckerr@gmail.com>
2021-10-07 18:39:06 -0400

    added frontend components and redux

---
+++ client/src/actions/posts.js
@@ -0,0 +1,24 @@
+import * as api from "../api";
+
+const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        
+        dispatch({ type: "FETCH_ALL", payload: data});
+    }catch(error) {
+        console.log(error.message);
+    }
+    
+   
+    
+    dispatch (action);
+}
+
+export const createPost = () => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+        distpatch ({type: "CREATE", payload: data });
+    }catch (error){
+        console.log(error);
+    }
+}
